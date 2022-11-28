2d5dc9583be4b31e672101f2a0f0c412d409b8d8
Ben_G <ben.goodwin.work1@gmail.com>
2022-11-28 10:20:53 +0000

    nav added and nested links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { Route, Switch, Redirect} from 'react-router-dom'
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
-    
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <Redirect to="quote-list" />
         </Route>
-      <Route path="/quote-list" exact>
-      <AllQuotes />
-      </Route>
-      <Route path="/quote-list/:quoteId">
-      <QuoteDetail />
-      </Route>
-      <Route path="/add-quote">
-      <NewQuote />
-      </Route>
+        <Route path="/quote-list" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quote-list/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/add-quote">
+          <NewQuote />
+        </Route>
       </Switch>
-    
+    </Layout>
   );
 }
 
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
-
+import { Route } from "react-router-dom";
+import Comments from '../components/comments/Comments'
 const QuoteDetail = () => {
   const params = useParams();
 
   return (
     <section>
       <h1>Quote {params.quoteId} Page</h1>
+      <section>
+        <Route path={`/quote-list/${params.quoteId}/comment`}>
+            <Comments />
+        </Route>
+      </section>
     </section>
   );
 };
